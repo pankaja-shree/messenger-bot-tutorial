7a6a8c062428538d8fab85396e4b8fad953a64e6
Pankajashree <pankajashree@gmail.com>
2017-05-07 14:50:05 +0530

    Add Postback functionality

--- index.js
+++ index.js
@@ -34,17 +34,18 @@ app.post('/webhook/', function (req, res) {
 		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
-			let text = event.message.text
-			if (text === 'Generic'){ 
-				//console.log("welcome to chatbot")
-				sendGenericMessage(sender)
+			let text = event.message.text.toLowerCase().trim()
+			if (text.substr(0,4) == 'wiki'){ 
+				//search wiki 
+				sendWikiResults(text.replace("wiki ",""),sender)
 				continue
 			}
-			sendHelp(sender)
+			else{
+				sendHelp(sender)
+			}
 		}
-		if (event.postback) {
-			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+		if (event.postback && event.postback.payload) {
+			sendTextMessage(sender, event.postback.payload)
 			continue
 		}
 	}
@@ -75,38 +76,73 @@ function sendHelp(sender) {
 	})
 }
 
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
+function sendWikiResults(query,sender) {
+
+	//save generic template format. Wiki page results to be pushed to elements array
+	let genericTemplate = {
+			attachment:{
+				type: "template",
+				payload: {
+					template_type: "generic",
+					elements: []
+				}
 			}
 		}
-	}
+	
+	//send GET request to wiki API. body contains the json object from API
+
+	const wikiUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=5&exlimit=max&gsrsearch='+query
+	request(wikiUrl, function(error, response, body){
+		if(error) console.log(error)
+		try{
+			body = JSON.parse(body)
+			let pages = body.query.pages
+			for(page of pages){
+				//Elements format - to push to elemebts array of Generic template 
+					let myElement = {
+						title: page.title,
+						subtitle: page.extract.substr(0,80).trim(),
+						buttons: [{
+							"type": "postback",
+							"title": "Read more",
+							"payload": page.extract.substr(0, 1000).trim()
+						},
+						{
+							"type": "web_url",
+							"url": "https://en.wikipedia.org/?curid=" + page.pageid,
+							"title": "View in browser"
+						}]
+					}
+					genericTemplate.message.attachment.payload.elements.push(myElement)		
+			}
+		}
+		catch(err) {
+			genericTemplate = {
+				"text": "Something went wrong, please try again."
+			}
+		}
+		//Post results to send API
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token:token},
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: genericTemplate,
+			}
+		}, function(error, response, body) {
+			if (error) {
+				console.log('Error sending messages: ', error)
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error)
+			}
+		})
+	})
+}
+
+function sendTextMessage(sender, msg) {
+	let messageData = { text:msg }
+	
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
